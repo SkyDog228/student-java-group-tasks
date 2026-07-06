16c39890b880c8324d4e2244f9b2e291e5605668
Anton Ivaneychik <116382042+SkyDog228@users.noreply.github.com>
2026-07-06 12:05:44 +0300

    Complete task

--- src/main/java/com/classroom/tasks/Task6SimpleTextEncoder.java
+++ src/main/java/com/classroom/tasks/Task6SimpleTextEncoder.java
@@ -3,14 +3,38 @@ package com.classroom.tasks;
 public class Task6SimpleTextEncoder {
 
     public static char shiftChar(char value, int shift) {
-        throw new UnsupportedOperationException("Students should implement this method");
+        shift %= 26;
+
+        if (value >= 'A' && value <= 'Z') {
+            return (char) ('A' + (value - 'A' + shift + 26) % 26);
+        }
+
+        if (value >= 'a' && value <= 'z') {
+            return (char) ('a' + (value - 'a' + shift + 26) % 26);
+        }
+
+        return value;
     }
 
     public static String encode(String text, int shift) {
-        throw new UnsupportedOperationException("Students should implement this method");
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < text.length(); i++) {
+            result.append(shiftChar(text.charAt(i), shift));
+        }
+
+        return result.toString();
     }
 
     public static int countLetters(String text) {
-        throw new UnsupportedOperationException("Students should implement this method");
+        int counter = 0;
+        for(int i = 0; i < text.length(); i++) {
+            char c = text.charAt(i);
+            if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')) {
+                counter++;
+            }
+            }
+        return counter;
+
     }
 }
